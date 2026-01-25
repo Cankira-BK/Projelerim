4465ce225b7c028daedd3cdd1611766b22857394
Cankira-BK <cankira@hotmail.com.tr>
2026-01-26 00:10:11 +0300

    proje dosyalari eklendi

--- sites/app/config/database.php
+++ sites/app/config/database.php
@@ -1,10 +1,10 @@
 <?php
-define('URL','https://www.website.com/');
+define('URL','http://127.0.0.1/mt2panel/');
 define('DB_TYPE','mysql');
 define('DB_HOST','localhost');
-define('DB_USER','database');
-define('DB_PASS','sifre');
-define('DB_NAME','databese');
+define('DB_USER','root');
+define('DB_PASS','123123123');
+define('DB_NAME','ogpanel');
 
 define('ACCOUNT_DB_NAME','account');
 define('COMMON_DB_NAME','common');
